a8515866a5b21c18c7710cb7159eeebcd257cbf9
Mihir Sawant <mih3190Sawant@outlook.com>
2022-07-15 10:18:12 +0530

    initial commit

---
+++ src/main/java/sawant/mihir/springbootkafkademo/SpringBootKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package sawant.mihir.springbootkafkademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaDemoApplication.class, args);
+	}
+
+}
